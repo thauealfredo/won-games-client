642b62d48120a292da2b43843d31770014251e90
Thaue Alfredo <thaue159@gmail.com>
2020-10-06 23:18:34 -0300

    add absolute path

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,6 +2,7 @@ module.exports = {
   stories: ['../src/components/**/stories.tsx'],
   addons: ['@storybook/addon-essentials'],
   webpackFinal: (config) => {
+    ///path absoluto
     config.resolve.modules.push(`${process.cwd()}/src`)
     return config
   }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,6 @@
+import GlobalStyles from 'styles/global';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles from '../src/styles/global';
-import { ThemeProvider } from 'styled-components';
-import theme from '../src/styles/theme';
+import theme from 'styles/theme';
  
 export const decorators = [
   (Story) => (
